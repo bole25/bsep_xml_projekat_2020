7e33b774ae0e8f7ac8525a5e2e2e3e6540c9fc18
bole25 <mbojan13@gmail.com>
2020-04-08 12:19:37 +0200

    Models for issuer data and subject data created

---
+++ src/main/java/com/xml_rent_a_car/model/data/IssuerData.java
@@ -0,0 +1,37 @@
+package com.xml_rent_a_car.model.data;
+
+import sun.security.x509.X500Name;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Date;
+
+public class IssuerData {
+
+    private X500Name x500Name;
+    private PrivateKey privateKey;
+
+    public IssuerData() {
+    }
+
+    public IssuerData(X500Name x500Name, PrivateKey privateKey) {
+        this.x500Name = x500Name;
+        this.privateKey = privateKey;
+    }
+
+    public X500Name getX500Name() {
+        return x500Name;
+    }
+
+    public void setX500Name(X500Name x500Name) {
+        this.x500Name = x500Name;
+    }
+
+    public PrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(PrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+}
